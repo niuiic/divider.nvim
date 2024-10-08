8fc4a1751c4e55d27e77d0513500cad6cf25a292
niuiic <niuiicde@gmail.com>
2024-10-08 12:31:07 +0800

    test: dividers

--- lua/divider/dividers/dividers.test.lua
+++ lua/divider/dividers/dividers.test.lua
@@ -1,6 +1,7 @@
 local Dividers = require("divider.dividers.dividers")
 local Divider = require("divider.dividers.divider")
 
+-- % Dividers:_get_sorted_dividers %
 local sorted_dividers = Dividers:_get_sorted_dividers({
 	Divider:new("", 10, 1, 1, {}),
 	Divider:new("", 1, 1, 1, {}),
@@ -12,3 +13,8 @@ local lnums = { 1, 2, 10, 30 }
 for i, sorted_divider in ipairs(sorted_dividers) do
 	assert(sorted_divider:get_lnum() == lnums[i])
 end
+
+-- % Dividers:find %
+local dividers = Dividers:new(sorted_dividers)
+assert(dividers:find(3):get_lnum() == 2)
+assert(dividers:find(2):get_lnum() == 2)
